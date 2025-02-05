8adc60b12419f1bb2c3d3a86511002e3a158d5c2
Evelina <Evelinabum123@gmail.com>
2025-02-05 13:21:06 +0200

    ff

--- js/intro/variables/06-numbers.js
+++ js/intro/variables/06-numbers.js
@@ -48,6 +48,93 @@ console.log(Math.ceil(10));
 
 
 console.log();
-console.log('Apvalinimas:round');
+console.log('"Apvalinimas": trunc') ;
 console.log(Math.round(10));
+console.log(Math.trunc(-9.001));
+
+console.log()
+console.log('"Apvalinimas":exp');
+console.log(Math.exp(2,2));
+console.log(Math.exp(3), Math.E ** 2);
+
+console.log()
+console.log('"Apvalinimas":pow');
+console.log(Math.pow(2,2),(2**2));
+
+
+console.log()
+console.log('"Apvalinimas":hypot');
+console.log(Math.hypot(3,4),   Math.sqrt(3** 2 + 4 **2));
+
+
+console.log();
+console.log('"Apvalinimas":min');
+console.log(Math.min(1,2,4,8, -1, 3.5));
+console.log(Math.min(1, NaN, -Infinity));
+
+console.log();
+console.log('"Apvalinimas":max');
+console.log(Math.max(1,2,4,8,-1, - Infinity, Infinity, 3.5,-2.7,));
+console.log(Math.max(1, NaN, -Infinity));
+
+console.log();
+console.log('"Apvalinimas":sign');
+console.log(Math.sign(5));
+console.log(Math.sign(-5));
+
+console.log();
+console.log('"Apvalinimas":random');
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+
+/**
+ * Generuojamas atsitiktinis skaicius intervale(imtinai).
+ * @param {*} min Nuo
+ * @param {*} max  Iki
+ * @returns {number} Atsitiktinis sveikasis skaicius
+ */
+
+
+
+function randomNumber(min,max) {
+    return Math.floor(Math.random() * (max-min+1)) +min;
+}
+
+for(let i=0; i<10; i++) {
+console.log(i, randomNumber(10,20));
+}
+
+
+//pasidaryti savo funkcija kuri grazina skaiciu su ribotu skaiciu kiekiu skaitmenu po kablelio
+//kas yra i++?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
