56db9c7ab25378521b52e7e197ff99d4ef068aea
Evelina <Evelinabum123@gmail.com>
2025-02-20 13:15:25 +0200

    up

--- js/loops/17-loops.js
+++ js/loops/17-loops.js
@@ -11,12 +11,43 @@ for(let i = 0; i < names.length; i++) {
     const name = names [i];
     console.log(name);
 }
+// i+=2 -kas antra
+//for(let i = 0; i < names.length; i+=2) {
 
 // FOR - OF
-for (const number of numbers) {
-    console.log(number);
+
+{
+    let i = 0;
+   for (const number of numbers) {
+        console.log(i++, '-->', number);
+    }
 }
 
-for(const name of names) {
-    console.log(name);
+{
+    let i = 0;
+    for(const name of names) {
+        console.log(i++, '-->',name);
+   }
+}
+
+// WHILE
+{
+    let i = 0;
+    while(i < 5) {
+        console.log(i,'while...');
+        i++;
+    }
+}
+
+// WHILE yra tas pats FOR:
+{
+for( let i= 0; i <5; i++) {
+        console.log(i,'for...');
+   }
+}
+
+{
+     for (let i = 0, j = 25; i < 20 && j > 10 ; i++, j--) {
+         console.log(i, j);
+    }
 }
