d1deb3d82411dece28c75dc8746f5297d26d6138
Evelina <Evelinabum123@gmail.com>
2025-02-26 12:52:01 +0200

    ip

--- js/functions/mini-digits.js
+++ js/functions/mini-digits.js
@@ -1,55 +1,55 @@
-function skaitmenuKiekisSkaiciuje (n) {
-  // parametru validacija
-  if(typeof n !== 'number' ) {
-    return 'Pateikta netinkamo tipo reiksme.';
-  }
-  if(isNaN(n) || n === Infinity || n === -Infinity ) {
-    return 'Pateiktas netinka skaiciaus tipo reiksme';
+function skaitmenuKiekisSkaiciuje(n) {
+    // parametru validacija
+    if (typeof n !== 'number') {
+        return 'Pateikta netinkamo tipo reikšmė.';
+    }
 
-  }
+    if (!isFinite(n)) {
+        return 'Pateiktas netinka skaičiaus tipo reikšmė.';
+    }
+    // alternatyva !isFinite(n)
+    // if (isNaN(n) || n === Infinity || n === -Infinity) {
+    //     return 'Pateiktas netinka skaičiaus tipo reikšmė.';
+    // }
 
-  // logika 
-  
+    // logika
     let result = ('' + n).length;
 
     if (n < 0) {
-      result--;
+        result--;
     }
+
     if (n % 1 !== 0) {
-      result--;
+        result--;
     }
 
-  // rezultato validacija
-
-  // rezultato grazinimas
-  return result;
+    // rezultato grazinimas
+    return result;
 }
 
+console.log(skaitmenuKiekisSkaiciuje(), '-->', 'Pateikta netinkamo tipo reikšmė.');
+console.log(skaitmenuKiekisSkaiciuje(undefined), '-->', 'Pateikta netinkamo tipo reikšmė.');
+console.log(skaitmenuKiekisSkaiciuje(true), '-->', 'Pateikta netinkamo tipo reikšmė.');
+console.log(skaitmenuKiekisSkaiciuje('asd'), '-->', 'Pateikta netinkamo tipo reikšmė.');
+console.log(skaitmenuKiekisSkaiciuje({}), '-->', 'Pateikta netinkamo tipo reikšmė.');
+console.log(skaitmenuKiekisSkaiciuje([]), '-->', 'Pateikta netinkamo tipo reikšmė.');
+console.log(skaitmenuKiekisSkaiciuje(skaitmenuKiekisSkaiciuje), '-->', 'Pateikta netinkamo tipo reikšmė.');
+console.log(skaitmenuKiekisSkaiciuje(null), '-->', 'Pateikta netinkamo tipo reikšmė.');
 
-console.log(skaitmenuKiekisSkaiciuje(), '-->', 'Pateikta netinkamo tipo  reiksme');
-console.log(skaitmenuKiekisSkaiciuje(undefined), '-->', 'Pateikta netinkamo  tipo reiksme');
-console.log(skaitmenuKiekisSkaiciuje(true), '-->', 'Pateikta netinkamo  tipo reiksme');
-console.log(skaitmenuKiekisSkaiciuje('asd'), '-->', 'Pateikta netinkamo  tipo reiksme');
-console.log(skaitmenuKiekisSkaiciuje({}), '-->', 'Pateikta netinkamo  tipo reiksme');
-console.log(skaitmenuKiekisSkaiciuje([]), '-->','Pateikta netinkamo  tipo reiksme');
-console.log(skaitmenuKiekisSkaiciuje(skaitmenuKiekisSkaiciuje), '-->', 'Pateikta netinkamo  tipo reiksme');
-console.log(skaitmenuKiekisSkaiciuje(null), '-->', 'Pateikta netinkamo  tipo reiksme');
-
-console.log(skaitmenuKiekisSkaiciuje(NaN), '-->', 'Pateiktas netinka skaiciaus tipo reiksme');
-console.log(skaitmenuKiekisSkaiciuje(Infinity), '-->', 'Pateiktas netinka skaiciaus tipo reiksme');
-console.log(skaitmenuKiekisSkaiciuje(-Infinity), '-->', 'Pateiktas netinka skaiciaus tipo reiksme');
+console.log(skaitmenuKiekisSkaiciuje(NaN), '-->', 'Pateiktas netinka skaičiaus tipo reikšmė.');
+console.log(skaitmenuKiekisSkaiciuje(Infinity), '-->', 'Pateiktas netinka skaičiaus tipo reikšmė.');
+console.log(skaitmenuKiekisSkaiciuje(-Infinity), '-->', 'Pateiktas netinka skaičiaus tipo reikšmė.');
 
 console.log(skaitmenuKiekisSkaiciuje(5), '-->', 1);
 console.log(skaitmenuKiekisSkaiciuje(781), '-->', 3);
 console.log(skaitmenuKiekisSkaiciuje(37060123456), '-->', 11);
 console.log(skaitmenuKiekisSkaiciuje(-5), '-->', 1);
 console.log(skaitmenuKiekisSkaiciuje(-781), '-->', 3);
-console.log(skaitmenuKiekisSkaiciuje(-37060123456), '-->',11);
+console.log(skaitmenuKiekisSkaiciuje(-37060123456), '-->', 11);
 console.log(skaitmenuKiekisSkaiciuje(3.14), '-->', 3);
-console.log(skaitmenuKiekisSkaiciuje(-3.14), '-->', 3);
 console.log(skaitmenuKiekisSkaiciuje(2.727), '-->', 4);
+console.log(skaitmenuKiekisSkaiciuje(-3.14), '-->', 3);
 console.log(skaitmenuKiekisSkaiciuje(-2.727), '-->', 4);
-console.log(skaitmenuKiekisSkaiciuje(1.000), '-->', 4);
 console.log(skaitmenuKiekisSkaiciuje(-1), '-->', 1);
 
 console.log(skaitmenuKiekisSkaiciuje(1_000_000_000), '-->', 10);
@@ -58,18 +58,7 @@ console.log(skaitmenuKiekisSkaiciuje(1_000_000_000_000_000), '-->', 16);
 console.log(skaitmenuKiekisSkaiciuje(1_000_000_000_000_000_000), '-->', 19);
 console.log(skaitmenuKiekisSkaiciuje(10_000_000_000_000_000_000), '-->', 20);
 console.log(skaitmenuKiekisSkaiciuje(100_000_000_000_000_000_000), '-->', 21);
-console.log(skaitmenuKiekisSkaiciuje(1000_000_000_000_000_000_000), '-->', 22);
-
-
-
-//kai virsija 22 funkcija nebepripazysta visu skaitmenu
-
-
-
-
-
-
-
-
-
-
+console.log(skaitmenuKiekisSkaiciuje(1_000_000_000_000_000_000_000), '-->', 22);
+console.log(skaitmenuKiekisSkaiciuje(1_000_000_000_000_000_000_000_000), '-->', 25);
+console.log(skaitmenuKiekisSkaiciuje(1_000_000_000_000_000_000_000_000_000), '-->', 28);
+console.log(skaitmenuKiekisSkaiciuje(1_000_000_000_000_000_000_000_000_000_000), '-->', 31);
